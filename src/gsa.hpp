729b37caeedf9fc1a79c1925ad5857798772e3ae
Kaiyuan Zhu <31667172+kyzhu@users.noreply.github.com>
2020-05-06 16:32:39 -0400

    Update gsa.hpp

--- src/gsa.hpp
+++ src/gsa.hpp
@@ -2,7 +2,6 @@
 #define GSA_HPP_
 
 #include <vector>
-//#include <ctime>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
@@ -15,13 +14,14 @@ class SuffixArray {
 		uint64_t *REV = NULL;
 
 		uint32_t *GSA = NULL;
-		uint16_t *LCP = NULL;
-		uint16_t *GLCP = NULL;
-
+		uint8_t *LCP = NULL;
+		uint8_t *LCP0 = NULL;
+		
 	public:
-		//std::clock_t start;
-		//double duration;
-		bool debug = false;
+		uint8_t *occ = NULL;
+		uint8_t *occ2 = NULL;
+		uint32_t *GSA2 = NULL;
+		
 		/* Constructors. */
 		SuffixArray(uint64_t);
 		~SuffixArray() {
@@ -33,34 +33,41 @@ class SuffixArray {
 				delete []GSA;
 			if (LCP != NULL)
 				delete []LCP;
-			if (GLCP != NULL)
-				delete []GLCP;
+			if (LCP0 != NULL)
+				delete []LCP0;
 		}
 
 		/* Compute the suffix array SA of s = s1$s2$...$sm;
 		   The suffix array of the string [s, s + n) is stored into SA[0, n]. */
-		void computeSuffixArray(uint8_t*, uint64_t);
+		void computeSuffixArray(uint8_t*, uint64_t, bool, bool);
 
 		/* Computes the reverse SA index.  REVSA[i] gives the index of the suffix
 		   starting a i in the SA array. */
- 		void computeRevSuffixArray(uint64_t);
+ 		void computeRevSuffixArray(uint64_t, bool);
 
 		/* Computes the generalized SA index.
 		   The id (1 <= id <= m) of each suffix is stroed into GSA[0, n]. */
-		void computeGnrSuffixArray(std::vector<uint64_t>&, std::vector<uint32_t>&, uint64_t);
+		void computeGnrSuffixArray(std::vector<uint64_t>&, std::vector<uint32_t>&, uint64_t, bool);
 
 		/* Computes the longest common prefix between adjacent suffixes. */
-		void computeLcpArray(uint8_t*, uint64_t);
+		void computeLcpArray(uint8_t*, uint64_t, bool, bool);
 
 		/**/
-		void computeGnrLcpArray(uint64_t);
-		void computeGnr2LcpArray(uint64_t);
+		void computeGnrLcpArray(uint64_t, bool);
+		void computeGnrLcpArray_d(uint64_t, bool);
+	
+		/**/
+		void computeGnrLcpArray_ext(uint64_t, uint8_t, bool);
+		void computeGnrLcpArray_dext(uint64_t, uint8_t, uint8_t, bool);
 
 		/**/
-		void computeMinUnique(uint64_t);
+		void computeMinUnique(uint64_t, bool);
 
-		uint64_t* run(uint8_t*, std::vector<uint64_t>&, std::vector<uint32_t>&, uint64_t);
+		uint64_t* run(uint8_t*, std::vector<uint64_t>&, std::vector<uint32_t>&, uint64_t, int, uint8_t, uint8_t, bool, bool, bool);
 
+		//uint64_t* run_ext_d(uint8_t*, std::vector<uint64_t>&, std::vector<uint32_t>&, uint64_t, int);
+
+		//std::pair<uint64_t*, uint64_t*> run_both(uint8_t*, std::vector<uint64_t>&, std::vector<uint32_t>&, uint64_t);
 };
 
 #endif
